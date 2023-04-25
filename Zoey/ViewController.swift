9ffca4d221d950967d12e2c58f36166a85347b3c
ibrahimahmedoglu <ibrahim.ahmedoglu@bahcesehir.edu.tr>
2023-04-25 12:01:25 +0300

    Initial Commit

---
+++ Zoey/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Zoey
+//
+//  Created by ibrahim ahmedoglu on 25.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
